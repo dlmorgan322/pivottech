540ce53e40e360dc6ef9f64c0328b78f5af73110
Devon Morgan <107596257+dlmorgan322@users.noreply.github.com>
2022-12-02 17:07:11 -0600

    Add files via upload

---
+++ calculator/calculator_test.go
@@ -0,0 +1,37 @@
+package calculator_test
+
+import (
+	"testing"
+
+	"github.com/dlmorgan322/pivottech.git/calculator"
+)
+
+//test
+
+func TestCalculator(t *testing.T) {
+
+	tests := map[string]struct {
+		x, y, want int
+		op         func(int, int) int
+		err        error
+	}{
+		"Add":  {x: 1, y: 2, want: 3, op: calculator.Add},
+		"Sub":  {x: 2, y: 1, want: 1, op: calculator.Sub},
+		"Mult": {x: 2, y: 3, want: 6, op: calculator.Mult},
+		"Div":  {x: 6, y: 2, want: 3, op: calculator.Div},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			if test.op != nil {
+				got := test.op(test.x, test.y)
+				if got != test.want {
+					t.Errorf("got %d, want %d", got, test.want)
+				}
+				return
+			}
+
+		})
+
+	}
+}
